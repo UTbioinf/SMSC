391f7ed491f6d9db554cc47f062ce659120afe16
zijuexiansheng <zijuexiansheng@gmail.com>
2019-06-05 01:40:09 -0400

    simplify installation method

--- depends/build_lemon/demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/loon/Loon/github/UTbioinf/SMSC/depends/lemon-1.3.1/demo
-# Build directory: /home/loon/Loon/github/UTbioinf/SMSC/depends/build_lemon/demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- depends/build_lemon/tools/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/loon/Loon/github/UTbioinf/SMSC/depends/lemon-1.3.1/tools
-# Build directory: /home/loon/Loon/github/UTbioinf/SMSC/depends/build_lemon/tools
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
